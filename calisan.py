73d97120aeee6b33c144c4825a91ea2ded4e7cd7
EmreSADAKATLI <Sadakatliemre@gmail.com>
2023-06-08 23:58:12 +0300

    get/set metodlarını tanımaldık

--- calisan.py
+++ calisan.py
@@ -9,4 +9,29 @@ class Calisan(Insan):
         self.__eski_maas = maas
         self.__yeni_maas = self.zam_hakki()
 
- 
+    def get_sektor(self):
+        return self.__sektor
+
+    def set_sektor(self, sektor):
+        self.__sektor = sektor
+
+    def get_tecrube(self):
+        return self.__tecrube
+
+    def set_tecrube(self, tecrube):
+        self.__tecrube = tecrube
+
+    def get_eski_maas(self):
+        return self.__eski_maas
+
+    def set_eski_maas(self, maas):
+        self.__eski_maas = maas
+
+    def get_yeni_maas(self):
+        return self.__yeni_maas
+    
+    def set_yeni_maas(self):
+        return self.zam_hakki()
+
+    
+   
